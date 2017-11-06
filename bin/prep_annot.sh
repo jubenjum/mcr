e5c7e34e638901f96c1cdedc0b30caa740fff2c9
Juan Benjumea <jubenjum@gmail.com>
2017-11-06 16:22:36 +0100

    fixed bug in bash scripts, they didnt worked with files that contains spaces as file names

--- bin/prep_annot.sh
+++ bin/prep_annot.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-DATA_DIR=$1
-OUTPUT_DIR=$2
+DATA_DIR="$1"
+OUTPUT_DIR="$2"
 
 # equivalent to $(readlink -f $1) in pure bash (compatible with macos)
 function realpath {
-    pushd $(dirname $1) > /dev/null
-    echo $(pwd -P)
-    popd > /dev/null
+    readlink -f "$1"
+    #pushd $(dirname $1) > /dev/null
+    #echo $(pwd -P)
+    #popd > /dev/null
 }
 export -f realpath;
 
@@ -17,36 +18,37 @@ export -f realpath;
 function failure { [ ! -z "$1" ] && echo "Error: $1"; exit 1; }
 
 
-# go to this script directory, restore current directory at exit
-trap "cd $(pwd)" EXIT
-
-
 # check if variables
 [ ! -z "$DATA_DIR" ] || failure "DATA_DIR command line argument not set" 
 [ ! -z "$OUTPUT_DIR" ] || failure "OUTPUT_DIR command line argument not set" 
 
-cd $DATA_DIR
+cd "$DATA_DIR"
 
 ## prepere output files 
-all_ann=$OUTPUT_DIR/annotations.csv
-train_ann=$OUTPUT_DIR/train_annotations.csv
-test_ann=$OUTPUT_DIR/test_annotations.csv
+all_ann="$OUTPUT_DIR"/annotations.csv
+train_ann="$OUTPUT_DIR"/train_annotations.csv
+test_ann="$OUTPUT_DIR"/test_annotations.csv
 rm -rf $train_ann $test_ann xaa xab
 
 # create a randomized and reproducible annotation file
 # FIX: lt is not reproducible
 RANDOM=1
-for tg_file in $DATA_DIR/*.TextGrid; do
-    text_name=$(basename $tg_file .TextGrid) # only the file name no directory
-    wav_name=$(ls -AF1 ${text_name}.* | grep -v TextGrid) # the other file
+for tg_file in "$DATA_DIR"/*.TextGrid; do
+    #echo "$tg_file" >&2 
+    text_name=$(basename "$tg_file" .TextGrid) # only the file name no directory
+    wav_name=$(ls -AF1 "$text_name".* | grep -v TextGrid) # the other file
     ext_wav_file="${wav_name##*.}"
-    curr_dir=$(realpath $tg_file)
+    curr_dir=$(realpath "$tg_file")
+    curr_name=$(echo $text_name | tr ' ' '_')
 
     #  
-    dump_textgrids.py $tg_file | grep -v filename | \
-        sed "s/TextGrid/"$ext_wav_file"/g" | \
-        sed "s#"$curr_dir"#"$OUTPUT_DIR/wav"#g" 
-done | shuf > $all_ann
+    dump_textgrids.py "$tg_file" | grep -v filename | \
+        sed "s/$text_name\.TextGrid/$curr_name\.wav/g" #| \
+        #sed "s#$curr_dir#$OUTPUT_DIR/wav#g" 
+done | shuf > "$all_ann"
+
+sed -i "s#$DATA_DIR#$OUTPUT_DIR/wav#g" "$all_ann" 
+
 
 # split will generate two files xaa with 80% of all data
 # and xab with the rest
@@ -61,6 +63,4 @@ cat xab >> $test_ann
 rm -rf xaa xab
 
 sed -i '1s/^/filename,start,end,label\n/' $all_ann
-
-cd -
 
--- bin/run_abx.sh
+++ bin/run_abx.sh
@@ -3,16 +3,16 @@
 set -e
 
 EXP_NAME=data
-BIN=$PWD/bin
+DATA_DIR="$1" 
+OUTPUT_DIR="$2"
 
-DATA_DIR=$1 
-OUTPUT_DIR=$2
 
 # equivalent to $(readlink -f $1) in pure bash (compatible with macos)   
 function realpath {                                                      
-    pushd $(dirname $1) > /dev/null                                      
-    echo $(pwd -P)                                                       
-    popd > /dev/null                                                     
+    readlink -f "$1"
+    #pushd $(dirname "$1") > /dev/null                                      
+    #echo $(pwd -P)                                                       
+    #popd > /dev/null                                                     
 }                                                                        
 export -f realpath;                                                      
                                                                          
@@ -20,54 +20,49 @@ export -f realpath;
 function failure { [ ! -z "$1" ] && echo "Error: $1"; exit 1; }          
                                                                          
                                                                          
-# go to this script directory, restore current directory at exit         
-trap "cd $(pwd)" EXIT                                                    
-                                                                         
-                                                                         
 # check if variables                                                     
 [ ! -z "$DATA_DIR" ] || failure "DATA_DIR command line argument not set"   
 [ ! -z "$OUTPUT_DIR" ] || failure "OUTPUT_DIR command line argument not set" 
 
-OUTPUT_PATH=$(realpath $OUTPUT_DIR)
-OUTPUT_NAME=$(basename $OUTPUT_DIR)
-OUTPUT_DIR=${OUTPUT_PATH}/${OUTPUT_NAME}
+OUTPUT_PATH=$(realpath "$OUTPUT_DIR")
+OUTPUT_DIR="${OUTPUT_PATH}"
 
-
-DATA_PATH=$(realpath $DATA_DIR)
-DATA_NAME=$(basename $DATA_DIR)
-DATA_DIR=${OUTPUT_PATH}/${DATA_NAME}
-
+DATA_PATH=$(realpath "$DATA_DIR")
+DATA_DIR="${DATA_PATH}"
 
 source activate mcr
 
 #
 ## remove previous outputs
 #
-rm -rf $OUTPUT_DIR/*.csv $OUTPUT_DIR/wav $OUTPUT_DIR/${EXP_NAME}.*
+rm -rf "$OUTPUT_DIR/*.csv" "$OUTPUT_DIR/wav" "$OUTPUT_DIR/${EXP_NAME}.*"
 
 #
 ## preparing data
 #
 
-mkdir -p $OUTPUT_DIR/wav
+mkdir -p "$OUTPUT_DIR/wav"
 echo "################"
 echo "# Preparing data"
 echo "################"
-cd $DATA_DIR
 
 # extracting annotations
-prep_annot.sh $DATA_DIR $OUTPUT_DIR 
+echo "doing prep_annot"
+prep_annot.sh "$DATA_DIR" "$OUTPUT_DIR" 
 
+cd "$DATA_DIR"
 # fixing sampling rate, wav files are in same directory than 
 # Praat TextGrid file
-for original_wav in $DATA_DIR/*.{WAV,wav}; do
-    new_wav=$OUTPUT_DIR/wav/$(basename $original_wav);
-    echo "$original_wav -> $new_wav" | tee -a $OUTPUT_DIR/${EXP_NAME}.log
-    sox -V0 -t wav "$original_wav" -e signed-integer -b 16 -c 1 \
-        -r 16000 "$new_wav" 2>&1 | tee -a $OUTPUT_DIR/${EXP_NAME}.log 
+echo "fixing wav files type"
+shopt -s nullglob
+for original_wav in "$DATA_DIR"/*.{WAV,wav,aif}; do
+    ext_file="${original_wav##*.}" 
+    new_wav="$OUTPUT_DIR/wav/"$(basename "$original_wav" .$ext_file | tr ' ' '_');
+    echo "$original_wav -> $new_wav" >> "${OUTPUT_DIR}/${EXP_NAME}".log
+    sox -V0 "$original_wav" -e signed-integer -b 16 -c 1 \
+        -r 16000 "$new_wav".wav 2>&1 >> "$OUTPUT_DIR/${EXP_NAME}".log 
 done
 
-cd -
 
 #
 ## training
@@ -85,7 +80,6 @@ cd -
 #python src/segmented_eval.py data/test_annotations.csv predicted.csv
 
 
-
 #
 ## create ABX files
 #
@@ -93,12 +87,14 @@ cd -
 echo "######################################"
 echo "# Preparing item and feature files ..."
 echo "######################################"
-prepare_abx.py  $OUTPUT_DIR/annotations.csv $OUTPUT_DIR/segmented.cfg $OUTPUT_DIR/$EXP_NAME 
+prepare_abx.py "$OUTPUT_DIR/annotations.csv" "$OUTPUT_DIR/segmented.cfg" "$OUTPUT_DIR/$EXP_NAME"
 
 source activate zerospeech
 echo "#################"
 echo "# Running ABX ..."
 echo "#################"
-run_abx.py $OUTPUT_DIR/$EXP_NAME
+run_abx.py "$OUTPUT_DIR/$EXP_NAME"
 source deactivate 
+
+echo "aca"
 
